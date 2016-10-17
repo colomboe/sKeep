38bc9c327b418e3207c2fa787425741ee0832391
Emanuele Colombo <emanuele.colombo@msec.it>
2016-10-17 08:20:44 +0200

    Added new dropbox library

---
+++ typings/dropbox-sdk-js.ts
@@ -0,0 +1,87 @@
+declare namespace DropboxSdkJs {
+
+    class Dropbox {
+        constructor(options: { clientId: string });
+        getAuthenticationUrl(redirectUri: string, state?: string): string;
+        authTokenRevoke(token: string): Promise<any>;
+        setAccessToken(token: string);
+        filesListFolder(arg: FileListFolderArgs): Promise<FilesListFolderResult>;
+        filesDownload(arg: 	FilesDownloadArg): Promise<FilesFileMetadata>;
+        filesGetMetadata(arg: FilesGetMetadataArg): Promise<FilesFileMetadata>;
+        filesUpload(args: FilesCommitInfo): Promise<FilesFileMetadata>;
+    }
+    
+    interface UserMessage {
+        text: string;
+        locale: string;
+    }
+
+    interface Error<T> {
+        error_summary: string;
+        error: T;
+        user_message: UserMessage;
+    }
+
+    interface FileListFolderArgs {
+        path: string;
+        recursive: boolean;
+        include_media_info: boolean;
+        include_deleted: boolean;
+        include_has_explicit_shared_members: boolean;
+    }
+
+    interface FilesPathRootError {
+        path_root: string;
+    }
+
+    interface FilesLookupError {
+        malformed_path: string;
+        invalid_path_root: FilesPathRootError; 
+    }
+
+    interface FilesListFolderError {
+        path: FilesLookupError;
+    }
+
+    interface FilesFileMetadata {
+        name: string;
+        id: string;
+        client_modified: string;
+        server_modified: string;
+        path_lower: string;
+        fileBlob: Blob;
+
+    }
+
+    interface FilesListFolderResult {
+        entries: FilesFileMetadata[];
+        cursor: string;
+        has_more: boolean;
+    }
+
+    interface FilesDownloadArg {
+        path: string;
+    }
+
+    interface FilesGetMetadataArg {
+        path: string;
+        include_media_info: boolean;
+        include_deleted: boolean;
+        include_has_explicit_shared_members: boolean;
+    }
+
+    interface FilesWriteMode {
+        ".tag": string;
+        update?: string;
+    }
+
+    interface FilesCommitInfo {
+        contents: Object;
+        path: string;
+        mode: FilesWriteMode;
+        autorename: boolean;
+        mute: boolean;
+    }
+}
+
+declare class Dropbox extends DropboxSdkJs.Dropbox {};
